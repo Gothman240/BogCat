4f6e376832506d9e1174a1ac537d819e2679931e
Gothman240 <paez.federico96@gmail.com>
2025-02-21 16:04:15 -0300

    first commit

---
+++ app/src/main/java/com/example/blogcat/data/model/Post.kt
@@ -0,0 +1,18 @@
+package com.example.blogcat.data.model
+
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.ServerTimestamp
+
+data class Post(
+    //@Exclude @JvmField
+    //var id: String = "",
+    //@ServerTimestamp
+    //var created_at: Date? = null,
+    val post_image: String = "",
+    val profile_picture: String = "",
+    val post_description: String = "",
+    //val poster: Poster? = null,
+    //val likes: Long = 0,
+    //@Exclude @JvmField
+    //var liked: Boolean = false)
+)
