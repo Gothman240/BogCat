5b38ba6a8e705f54c938bf9b38de0bca609580ac
Gothman240 <paez.federico96@gmail.com>
2025-02-24 18:20:48 -0300

    add register fragment and layout, update login fragment and navigation

--- app/src/main/java/com/example/blogcat/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/blogcat/ui/auth/LoginFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.blogcat.R
+import com.example.blogcat.core.Resource
+import com.example.blogcat.data.remote.auth.LoginDataSource
 import com.example.blogcat.databinding.FragmentLoginBinding
+import com.example.blogcat.domain.auth.LoginRepoImpl
+import com.example.blogcat.presentation.auth.LoginScreenViewModel
+import com.example.blogcat.presentation.auth.LoginScreenViewModelFactory
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -15,10 +23,16 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding: FragmentLoginBinding
     private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val viewModel by viewModels<LoginScreenViewModel> { LoginScreenViewModelFactory(LoginRepoImpl(
+        LoginDataSource()
+    )) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
+        isUserLoggedIn()
+        doLogin()
+        goToSingUpPage()
     }
     
     private fun isUserLoggedIn(){
@@ -36,8 +50,30 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         }
     }
 
-    private fun signIn(email: String, password: String) {
+    private fun goToSingUpPage(){
+        binding.textSignup.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
 
+    private fun signIn(email: String, password: String) {
+        viewModel.signIn(email, password).observe(viewLifecycleOwner, Observer { result ->
+            when(result){
+                is Resource.Loading ->{
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.btnSignin.isEnabled = false
+                }
+                is Resource.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    findNavController().navigate(R.id.action_loginFragment_to_homeScreenFragment)
+                }
+                is Resource.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.btnSignin.isEnabled = true
+                    Toast.makeText(requireContext(), "Error: ${result.exception}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 
     private fun validateCredentials(email: String, password: String) {
